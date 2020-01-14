c5d802395bce5aa20d4f6565afbf46e52433ff7b
lpolon <lpolon@gmail.com>
2020-01-14 13:31:45 -0300

    added 29 game seeds from querying giantbomb database. it will be easier to add new games in the future.

--- models/users.js
+++ models/users.js
@@ -13,6 +13,7 @@ const UserSchema = new Schema(
       enum: ['morning', 'evening', 'afternoon', 'ALLNIGHT'],
     },
     // TODO: testar
+    // https://www.youtube.com/watch?v=9JZJsChpwKs&list=WL&index=29&t=0s
     friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     games: [{ type: Schema.Types.ObjectId, ref: 'Games' }],
   },
