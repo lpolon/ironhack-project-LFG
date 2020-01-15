17cf66792df5f268d74c3f984da501f771724c0e
lpolon <lpolon@gmail.com>
2020-01-15 17:52:01 -0300

    ./games is now called find-games for clarity

--- routes/username-games.js
+++ routes/username-games.js
@@ -6,7 +6,7 @@ const isUserGameArrEmpty = require('../middlewares/isUserGameArrEmpty');
 myGamesRouter.get('/', isUserGameArrEmpty, async (req, res, next) => {
   const { status } = req.noGamesFlag;
   if (status) {
-    res.redirect('/games');
+    res.redirect('/find-games');
   }
   const { games } = await Users.findById(req.user._id).populate('games');
   const { username } = req.user;
